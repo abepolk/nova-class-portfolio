151c965beae007544b9885592adffcf349120b1f
Abraham Polk <abepolk@gmail.com>
2020-06-24 11:29:11 -0400

    Initial commit

---
+++ test_frontend/pages/about.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+const About = () => {
+    return (
+        <>
+<Layout>
+            <h1>Hello from me About</h1>
+</Layout>        </>
+    )
+};
+export default About;
---
+++ test_frontend/pages/contact.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+const Contact = () => {
+    return (
+        <>
+            <Layout>
+            <h1>Hello Contact</h1>
+</Layout>        </>
+    )
+};
+export default Contact;
