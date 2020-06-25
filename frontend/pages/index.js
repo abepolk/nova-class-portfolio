84c83515f88f8f4ea0802cea08748c83566f521b
Abraham Polk <abepolk@gmail.com>
2020-06-25 14:26:18 -0400

    Some changes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from 'components/Card/Card'
+import fetch from 'isomorphic-unfetch'
 
 export default function Index () {
     return (
@@ -9,4 +10,18 @@ export default function Index () {
             </Card>
         </div>
     )
+}
+
+Index.getInitialProps = async () => {
+    let res, data, projects;
+    try{
+        res = await fetch('http://161.35.7.148:8000/api/v1/project/');
+        data = await res.json();
+        projects = data.objects;
+    } catch(error){
+        console.error(error)
+    }
+    return {
+        projects
+    }
 }
