1928b42734242efadb5f8939504d035be4dab0a5
Abraham Polk <abepolk@gmail.com>
2020-06-25 11:13:05 -0400

    Class work Thursday

--- portfolio_django/portfolio_django/settings.py
+++ portfolio_django/portfolio_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,13 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wx2j#hzl+zp@f)&5ron7gvo^=5w3$0(s6alr9)l@xp-&!rq&r='
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '161.35.7.148/']
 
 
 # Application definition
